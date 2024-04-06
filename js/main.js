3a7e71baf6d495672467dd2f322dc8d65902d5ea
apte-ame <apte.ame@northeastern.edu>
2024-04-06 12:47:28 -0400

    Updated typesd.js scroll intro text

--- js/main.js
+++ js/main.js
@@ -94,10 +94,10 @@
     var typed_strings = $('.text-slider-items').text();
 		var typed = new Typed('.text-slider', {
 			strings: typed_strings.split(','),
-			typeSpeed: 80,
+			typeSpeed: 30,
 			loop: true,
 			backDelay: 1100,
-			backSpeed: 30
+			backSpeed: 40
 		});
 	}
 
